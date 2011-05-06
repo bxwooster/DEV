9b0d53a5b37ce76d5f951a491c0ff80fe586ee54
bwooster <kuhnoo@gmail.com>
2011-05-06 22:00:48 +0400

    NoEffect: point lights done.

--- source/RenderLights.cpp
+++ source/RenderLights.cpp
@@ -63,11 +63,10 @@ void RenderLights(GraphicsState& state, VisualRenderInfo& vinfo, LightRenderInfo
 
 		for (uint i = 0; i < casters.size(); i++)
 		{
-			Visual& caster = casters[i];
-			Geometry& geom = vinfo.geoms[caster.type];
+			Geometry& geom = vinfo.geoms[casters[i].type];
 
 			CBufferLayouts::object_z data;
-			data.world_lightview_lightproj = lightview_lightproj * transforms[caster.index];
+			data.world_lightview_lightproj = lightview_lightproj * transforms[casters[i].index];
 
 			state.context->UpdateSubresource(cb_object_z, 0, NULL, (void*)&data, sizeof(data), 0);
 			state.context->IASetVertexBuffers(0, 1, &geom.buffer, &geom.stride, &geom.offset);
@@ -105,60 +104,63 @@ void RenderLights(GraphicsState& state, VisualRenderInfo& vinfo, LightRenderInfo
 	{
 		Light& light = lights.point[k];
 
-		state.context->IASetInputLayout( vinfo.layout );
-		state.context->IASetPrimitiveTopology( D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST );
-
 		Matrix4f lightview = view_axis * transforms[light.index].inverse();
 		Matrix4f lightview_lightproj = info.proj * lightview;
 
-		Matrix4f light_matrix = lightview * camera.view.inverse();
-		HOK( state.var.light_matrix->SetMatrix( light_matrix.data() ));
-		Matrix4f reproject = info.proj * light_matrix;
-		HOK( state.var.reproject->SetMatrix( reproject.data() ));
-
-		state.context->ClearDepthStencilView
-			(shadowcube.dsv, D3D11_CLEAR_DEPTH, 1.0, 0);
-
-		auto v = shadowcube.viewport;
+		D3D11_VIEWPORT& v = shadowcube.viewport;
 		D3D11_VIEWPORT viewports[6] = {v, v, v, v, v, v};
+
+		state.context->ClearDepthStencilView(shadowcube.dsv, D3D11_CLEAR_DEPTH, 1.0, 0);
+		state.context->OMSetRenderTargets(0, NULL, shadowcube.dsv);
+		state.context->IASetInputLayout( vinfo.layout );
+		state.context->IASetPrimitiveTopology( D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST );
 		state.context->RSSetViewports( 6, viewports );
+		state.context->GSSetConstantBuffers(3, 1, &cb_object_cube_z);
+
+		state.context->VSSetShader( info.vs_render_cube_z, NULL, 0 );
+		state.context->GSSetShader( info.gs_render_cube_z, NULL, 0 );
+		state.context->PSSetShader( NULL, NULL, 0 );
+		state.context->RSSetState( info.rs_shadow );
+		state.context->OMSetBlendState( NULL, blendf, 0xffffffff );
+		state.context->OMSetDepthStencilState( NULL, 0u );
 
 		for (uint i = 0; i < casters.size(); i++)
 		{
-			{
-				Matrix4f p = info.proj;
-				Matrix4f w = lightview * transforms[casters[i].index];
-				Matrix4f cubeproj[6];
-				for (int f = 0; f < 6; f++) cubeproj[f] = p * info.cubematrices[f] * w;
-				HOK( state.var.cubeproj->SetMatrixArray((float*) cubeproj, 0, 6));
-			}
-				
-			HOK( state.var.shadowcube->SetResource( NULL ) );
-			HOK( state.pass_render_cube_z->Apply( 0, state.context ) );
-			state.context->OMSetRenderTargets(0, NULL, shadowcube.dsv);
-		
 			Geometry& geom = vinfo.geoms[casters[i].type];
 
-			state.context->IASetVertexBuffers
-				(0, 1, &geom.buffer, &geom.stride, &geom.offset);
+			Matrix4f& p = info.proj;
+			Matrix4f w = lightview * transforms[casters[i].index];
+			CBufferLayouts::object_cube_z data;
+			for (int f = 0; f < 6; f++) data.cubeproj[f] = p * info.cubematrices[f] * w;	
+
+			state.context->UpdateSubresource(cb_object_cube_z, 0, NULL, (void*)&data, sizeof(data), 0);
+			state.context->IASetVertexBuffers(0, 1, &geom.buffer, &geom.stride, &geom.offset);
 			state.context->Draw( geom.count, 0 );
 		}
 
+		CBufferLayouts::light data;
+		data.light_matrix = lightview * camera.view.inverse();
+		data.reproject = info.proj * data.light_matrix;
+		data.light_pos = (camera.view * transforms[light.index].col(3)).head<3>();
+		data.light_colour = light.colour;
+
+		ID3D11Buffer* buffers[5] = { cb_frame, NULL, NULL, NULL, cb_light };
+		ID3D11ShaderResourceView* views[5] = { gbuffer0.srv, gbuffer1.srv, zbuffer.srv, NULL, shadowcube.srv };
+
+		state.context->UpdateSubresource(cb_light, 0, NULL, (void*)&data, sizeof(data), 0);
+		state.context->OMSetRenderTargets(1, &lbuffer.rtv, NULL);
 		state.context->IASetInputLayout( NULL );
 		state.context->IASetPrimitiveTopology( D3D11_PRIMITIVE_TOPOLOGY_POINTLIST );
 		state.context->RSSetViewports( 1, &lbuffer.viewport );
-		state.context->OMSetRenderTargets(1, &lbuffer.rtv, NULL);
+		state.context->PSSetConstantBuffers(0, 5, buffers);
+		state.context->PSSetShaderResources(0, 5, views);
 
-		Vector4f position(camera.view * transforms[light.index].col(3));
-		HOK( state.var.light_pos->SetRawValue
-			( (void*)position.data(), 0, sizeof(Vector3f) ) );
-
-		Vector3f& colour = light.colour;
-		HOK( state.var.light_colour->SetRawValue
-			( (void*)colour.data(), 0, sizeof(Vector3f) ) );
-		
-		HOK( state.var.shadowcube->SetResource( shadowcube.srv ) );
-		HOK( state.pass_point_light->Apply( 0, state.context ) );
+		state.context->VSSetShader( info.vs_noop, NULL, 0 );
+		state.context->GSSetShader( info.gs_fullscreen, NULL, 0 );
+		state.context->PSSetShader( info.ps_point_light, NULL, 0 );
+		state.context->OMSetBlendState( info.bs_additive, blendf, 0xffffffff );
+		state.context->OMSetDepthStencilState( info.ds_nowrite, 0 );
+		state.context->RSSetState( vinfo.rs_default );
 
 		state.context->Draw( 1, 0 );
 	}
